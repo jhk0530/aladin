8f5ea144f5782881d81bf57a51ac0fe440acb478
JH Kim <hwanistic@gmail.com>
2020-10-16 12:43:52 +0900

    Update main.py
    1. 잠실새내역점 추가
    2. 출간일순 -> 등록일 순으로 변경

--- main.py
+++ main.py
@@ -12,19 +12,26 @@ if __name__ == "__main__":
   seoul_timezone = timezone('Asia/Seoul')
   today = datetime.now(seoul_timezone)
   today_data = today.strftime("%Y년 %m월 %d일")
-  aladin_url = "https://www.aladin.co.kr/usedstore/wbrowse.aspx?offcode=Jamsil&ItemType=0&BrowseTarget=AllView&ViewRowsCount=50&ViewType=Detail&PublishMonth=24&SortOrder=5&page=1&PublishDay=84&CID=351&SearchOption=&IsDirectDelivery=&QualityType=&OrgStockStatus="
+  aladin_url = "https://www.aladin.co.kr/usedstore/wbrowse.aspx?offcode=Jamsil&ItemType=0&BrowseTarget=AllView&ViewRowsCount=50&ViewType=Detail&PublishMonth=24&SortOrder=6&page=1&PublishDay=84&CID=351&SearchOption=&CustReviewRankStart=&CustReviewRankEnd=&CustReviewCountStart=&CustReviewCountEnd=&PriceFilterMin=&PriceFilterMax=&IsDirectDelivery=&QualityType=&OrgStockStatus="
 
   res = requests.get(aladin_url).text
   soup = BeautifulSoup(res, features = 'html.parser')
   titles = soup.find_all("a", {"class":"bo_l"})
   titles2 = [str(i.text.split('<b>')).replace("[","").replace("]","").replace("'","") for i in titles]
   titles2 = '- ' + "\n- ".join(titles2)
-  issue_title = f"알라딘 중고서점 잠실점 IT 신간 도서 알림({today_data})"
+  issue_title = f"알라딘 잠실롯데월드타워점 새로 등록된 IT 도서 알림({today_data})"
   repo = get_github_repo(access_token, repository_name)
-  upload_github_issue(repo, issue_title, titles2)
-  print("Upload Github Issue Success!")
+  upload_github_issue(repo, issue_title, titles2)  
 
-  
-  
+  aladin_url = "https://www.aladin.co.kr/usedstore/wbrowse.aspx?offcode=Sincheon&ItemType=0&BrowseTarget=AllView&ViewRowsCount=50&ViewType=Detail&PublishMonth=24&SortOrder=6&page=1&PublishDay=84&CID=351&SearchOption=&IsDirectDelivery=&QualityType=&OrgStockStatus="
+   
+  res = requests.get(aladin_url).text
+  soup = BeautifulSoup(res, features = 'html.parser')
+  titles = soup.find_all("a", {"class":"bo_l"})
+  titles2 = [str(i.text.split('<b>')).replace("[","").replace("]","").replace("'","") for i in titles]
+  titles2 = '- ' + "\n- ".join(titles2)
+  issue_title = f"알라딘 잠실새내역점 새로 등록된 IT 도서 알림({today_data})"
+  repo = get_github_repo(access_token, repository_name)
+  upload_github_issue(repo, issue_title, titles2)  
 
 
